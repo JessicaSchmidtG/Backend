48ebb7bc7390aef7b0e089dc8448175eb9380797
Jéssica Schmidt <jessyka_sl@hotmail.com>
2023-06-21 20:48:29 -0300

    Introdução Typescript

---
+++ introducao/script.ts
@@ -0,0 +1,40 @@
+//DECLARAÇÃO DE VARIAVEIS
+const company: string = "Senac";
+let age: number = 31;
+let isCorrect: boolean = true;
+
+//DECLARAÇÃO DE ARRAY
+const arr: Array<number> = [1, 2, 3];
+const array: number[] = [1,2,3];
+
+//DECLARAÇÃO DE OBJETO
+const person: {
+    name: string,
+    age: number
+} = {
+    name: "Senac",
+    age: 30
+}
+
+//DECLARAÇÃO TIPO ANY
+let result: any;
+result = "result";
+result = 1903;
+result = true;
+
+//DECLARAÇÃO FUNÇÃO TIPADA
+function sum(n1: number, n2: number): number{
+    return n1 + n2;
+}
+
+function resultSum(n1: number, n2: number): string{
+    let result = n1 + n2;
+    return `Resultado é ${result}.`;
+}
+
+//DECLARÇÃO DE FUNÇÃO VOID
+function sayHello(name?: string): void {
+    console.log("Hello, ", name || "world");
+}
+
+

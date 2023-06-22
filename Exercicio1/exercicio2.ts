d92d14f5dbba97639a3140d15afb436ad99b4d30
Jéssica Schmidt <jessyka_sl@hotmail.com>
2023-06-21 21:59:54 -0300

    Exercicio 3

---
+++ Exercicio1/exercicio2.ts
@@ -0,0 +1,6 @@
+function printThreeFavoriteColors(): void {
+    const color1 = prompt("Insira sua primeira cor favorita")
+    const color2 = prompt("Insira sua segunda cor favorita")
+    const color3 = prompt("Insira sua terceira cor favorita")
+    console.log([color1, color2, color3])
+ }

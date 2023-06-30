92af1cce10b5ef596531dac803274997469259d9
Jéssica Schmidt <jessyka_sl@hotmail.com>
2023-06-29 21:53:42 -0300

    exercicios

---
+++ Exercicio10/src/exercicio10.ts
@@ -0,0 +1,35 @@
+// entrada
+let users =[
+	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+]
+type clients = {
+    cliente: string,
+    saldoTotal: number,
+    debitos: number[],
+}
+
+function getNegativeAmount(users: clients[]): clients[] {
+//chamada da função para atualizar saldo total
+    calculateDebits();
+    
+    return users.filter(clients => {
+        return clients.saldoTotal <0
+    });    
+}
+
+function calculateDebits() {
+    return users.map(user => {
+        const sumDebits = user.debitos.reduce((acumulator:number, value:number)=>
+            value ? acumulator += value : 0
+        , 0);
+        user.saldoTotal -= sumDebits
+        user.debitos = [];
+    });
+}
+
+console.log(getNegativeAmount(users));
---
+++ Exercicios/build/exercicios.js
@@ -0,0 +1,18 @@
+"use strict";
+let users = [
+    { name: "Rogério", email: "roger@email.com", role: "user" },
+    { name: "Ademir", email: "ademir@email.com", role: "admin" },
+    { name: "Aline", email: "aline@email.com", role: "user" },
+    { name: "Jéssica", email: "jessica@email.com", role: "user" },
+    { name: "Adilson", email: "adilson@email.com", role: "user" },
+    { name: "Carina", email: "carina@email.com", role: "admin" }
+];
+let adm = users.filter((user) => {
+    return user.role === "admin";
+});
+const emails = adm.map((mapEmail) => {
+    return mapEmail.email;
+});
+console.log(adm);
+console.log(emails);
+//# sourceMappingURL=exercicios.js.map
